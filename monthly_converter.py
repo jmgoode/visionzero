2b1888c2c0f7c8751eb0166194ece1f17f5b3873
Jordan Zandi <jtzandi@cs.uchicago.edu>
2018-05-11 11:02:46 -0500

    great description

--- monthly_converter.py
+++ monthly_converter.py
@@ -8,7 +8,7 @@ writer = csv.writer(outfile)
 
 header = next(reader,None) #skip first line
 print(header)
-writer.writerow(('FID', 'Join_Count', 'TARGET_FID','Fatalities','PedFatalit','BikeFatali','MVOFatalit','MN','YR','NODEID','Lon,Lat','STREET1','STREET2'))
+writer.writerow(('FID', 'Join_Count', 'TARGET_FID','Fatalities','PedFatalit','BikeFatali','MVOFatalit','MN','YR','NODEID','Lon','Lat','STREET1','STREET2'))
 
 input = Proj(init='epsg:2263', preserve_units=True)
 output = Proj(init='epsg:4326')
@@ -17,7 +17,7 @@ for row in reader:
     x1 = row[10]
     y1 = row[11]
     lon,lat = transform(input,output,x1,y1)
-    writer.writerow((row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],(lon,lat),row[12],row[13]))
+    writer.writerow((row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],lon,lat,row[12],row[13]))
 
 infile.close()
 outfile.close()
